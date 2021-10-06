31e5c35a44ff0fb08de6fc5d8397257d1eb651cd
Muhammad Uzair <uzairkhattak004@gmail.com>
2021-10-07 01:07:06 +0500

    🗃️ models added via migrations

---
+++ migrations/20211006034824-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('Users', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			phone_number: {
+				type: Sequelize.INTEGER
+			},
+			otp: {
+				type: Sequelize.INTEGER(4).UNSIGNED
+			},
+			otp_expiration_date: {
+				type: Sequelize.DATE
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		})
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('Users');
+	}
+};
---
+++ models/user.js
@@ -0,0 +1,46 @@
+const moment = require('moment');
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	class User extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	};
+	User.init({
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		phone_number: {
+			type: DataTypes.INTEGER
+		},
+		otp: {
+			type: DataTypes.INTEGER(4).UNSIGNED
+		},
+		otp_expiration_date: {
+			type: DataTypes.DATE,
+			set(value) {
+				// convert regular Date to moment Date
+				value = moment(value).add(5, 'minutes');
+				this.setDataValue('otp_expiration_date', value);
+			}
+		},
+		is_otp_expired: {
+			type: DataTypes.VIRTUAL,
+			get() {
+				// otp_expiration_date < current date
+				return this.getDataValue(otp_expiration_date).isAfter(moment()) ? true : false
+			}
+		}
+	}, {
+		sequelize,
+		modelName: 'User',
+	});
+	return User;
+};
