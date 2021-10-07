9f894f7a4e0d6b89530f748ffa146c19a7c81791
Muhammad Uzair <uzairkhattak004@gmail.com>
2021-10-07 23:54:10 +0500

    ✨ generate OTP 🐛 otp bug fix ➕ multer added
    	1. otp genrated at wrong route bug fixed
    	2. generate OTP with expiration date

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,6 @@ const User = require('../models/user')(sequelize, Sequelize.DataTypes, Sequelize
 exports.createUser = async function (req, res) {
 
 	const { name, phone_number } = req.body;
-	const otp = Math.floor(1000 + Math.random() * 9000);
 	const currentDate = new Date();
 
 	const newUser = { name, phone_number, otp, otp_expiration_date: currentDate }
@@ -22,8 +21,30 @@ exports.createUser = async function (req, res) {
 
 }
 
-exports.generateOTP = function (req, res) {
-	res.json({ msg: 'you are on generateOTP route' });
+exports.generateOTP = async function (req, res) {
+
+	let foundUser;
+
+	try {
+		foundUser = await User.findOne({ where: { phone_number: req.body.phone_number } });
+	} catch (findErr) {
+		console.error('error while finding user: ', findErr);
+		res.json({ statusCode: 500, msg: 'Server error' });
+		return;
+	}
+
+	if (foundUser) {
+		const otp = Math.floor(1000 + Math.random() * 9000);
+		foundUser.otp = otp;
+		foundUser.otp_expiration_date = new Date();
+
+		await foundUser.save();
+		res.json({ statusCode: 200, id: foundUser.id });
+
+	} else {
+		res.json({ statusCode: 404, msg: 'Phone Number doesnt exist' });
+	}
+
 }
 
 exports.verifyOTP = function (req, res) {
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// handles multipart/form-data
+const multer = require('multer');
+const upload = multer();
+
 const userController = require('../controllers/user.js');
 
 // create user route
 router.post('/', userController.createUser);
 
 // generate otp route
-router.post('/generateOTP', userController.generateOTP);
+router.post('/generateOTP', upload.none(), userController.generateOTP);
 
 // verify otp route
 router.post('/:id/verifyOTP', userController.verifyOTP);
